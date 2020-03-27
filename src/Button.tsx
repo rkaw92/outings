f1655cc7ccfc70b939dae868df5d9522cef1af0e
Robert Kawecki <robert+github@rkaw.pl>
2020-03-28 00:18:03 +0100

    Remove everything from gh-pages to prepare for dist/ deploys

--- src/Button.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FunctionComponent } from 'react';
-
-const Button: FunctionComponent<{ text: string }> = function Button({ text }) {
-    return <button className="block mx-auto p-2 px-8 m-4 border border-blue-500 rounded text-2xl text-blue-500 font-medium">{text}</button>;
-};
-
-export { Button };
